e625feddc2b92771c46f32cba753d58e3f94484a
William Rusli <wrusli75@gmail.com>
2023-10-29 23:46:38 +0700

    fix navbar

--- resources/views/layouts/bottomNav.blade.php
+++ resources/views/layouts/bottomNav.blade.php
@@ -1,7 +1,6 @@
 <!-- App Bottom Menu -->
-<link rel="stylesheet" href={{ asset("assets/css/style-dashboard.css") }}  />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 <div class="appBottomMenu" >
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <div class="container-fluid bottom-bar bg-white d-flex justify-content-evenly align-items-center">
         <a href="/dashboard" class="d-flex flex-column justify-content-center align-items-center menu {{ request()->is('dashboard') ? 'active' : '' }}">
           <img width="25" height="25" src="../../assets/img/home.png" alt="" />
