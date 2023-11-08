bcf8da13fe8ea79dc1acac85735717e176989a1f
My Name (alfiannnas) <me@example.com>
2023-11-08 09:44:07 +0700

    update sidebar and footer fe

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -9,11 +9,11 @@
           <ul class="list-inline list-inline-dots mb-0">
             <li class="list-inline-item">
               Copyright &copy; 2023
-              <a href="." class="link-secondary">PT Ressa Abadi</a>.
+              <a href="" class="link-secondary">PT Ressa Abadi</a>.
               All rights reserved.
             </li>
             <li class="list-inline-item">
-              <a href="./changelog.html" class="link-secondary" rel="noopener">
+              <a href="https://www.itera.ac.id/" class="link-secondary" rel="noopener">
                 Institut Teknologi Sumatera
               </a>
             </li>
