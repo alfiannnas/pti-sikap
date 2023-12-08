08624ac02f099e186e7f48dc38beafb2a9951d98
Ferdy <51710824+ferdym02@users.noreply.github.com>
2023-12-08 19:01:22 +0700

    Update cetaklaporan.blade.php

--- resources/views/presensi/cetaklaporan.blade.php
+++ resources/views/presensi/cetaklaporan.blade.php
@@ -122,18 +122,21 @@
     <p>Keterangan</p>
     <div class="container">
       <div>
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M5 12l5 5l10 -10"></path>
-      </svg>
-      <span>Hadir</span>
-      </div>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M5 12l5 5l10 -10"></path>
+        </svg>
+        <span>Hadir</span>
+        </div>
       <div style="margin-top:20px;margin-bottom:20px">
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="red" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M5 12l5 5l10 -10"></path>
-      </svg>
-      <span>Cuti</span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="red" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M5 12l5 5l10 -10"></path>
+        </svg>
+        <span>Cuti</span>
+      </div>
+      <div style="margin-bottom:20px">
+      <span>T</span><span style="margin-left:7px">Telat</span>
       </div>
     </div>
     <button id="printButton" class="btn btn-primary btn-print">Cetak Laporan</button>
